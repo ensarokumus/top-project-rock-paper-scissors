1c2f573bec193653a7dfb53ec626b983b702e9f7
ensarokumus <41113027+ensarokumus@users.noreply.github.com>
2023-07-19 23:06:00 +1000

    Create a function to play Rock Paper Scissors
    Create a function named playRound that plays a single round of Rock
    Paper Scissors to take two parameters for the player's and computer's
    selections.

--- javascript.js
+++ javascript.js
@@ -1,13 +1,29 @@
 let randomNumber = Math.random();
+let computerSelection = '';
+let playerSelection = prompt('Do you choose Rock, Paper or Scissors?', '')
 
 function getComputerChoice() {
     if (randomNumber < 0.333) {
-        console.log('Rock');
+        computerSelection = 'Rock';
     } else if (randomNumber >= 0.666) {
-        console.log('Scissor');
+        computerSelection = 'Scissor';
     } else {
-        console.log('Paper');
+        computerSelection = 'Paper';
     }
 }
 
 getComputerChoice();
+
+function playRound(playerSelection, computerSelection) { 
+    if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) { 
+        return 'It\'s a tie!';
+    } else if (playerSelection.toLowerCase() === 'rock' & computerSelection.toLowerCase() === 'scissor' || 
+        playerSelection.toLowerCase() === 'paper' & computerSelection.toLowerCase() === 'rock' || 
+        playerSelection.toLowerCase() === 'scissor' & computerSelection.toLowerCase() === 'paper') { 
+        return `You Win! ${playerSelection.slice(0, 1).toUpperCase()}${playerSelection.slice(1).toLowerCase()} beats ${computerSelection}`;
+    } else { 
+        return `You Lose! ${computerSelection} beats ${playerSelection.slice(0, 1).toUpperCase()}${playerSelection.slice(1).toLowerCase()}`;
+    }
+}
+
+console.log(playRound(playerSelection, computerSelection));
