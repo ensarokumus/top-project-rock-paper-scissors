88521fec0964f144397787d12e42b42f2be96d44
ensarokumus <41113027+ensarokumus@users.noreply.github.com>
2023-07-20 21:41:09 +1000

    Create a function to play 5 rounds of the game Create a function called game() to play a 5 round game that keeps score and report a winner or loser at the end by using the playRound function that was created in the previous commit. Do not allow a player to enter any other prompts other than "Rock", "Paper" and "Scissors".

--- javascript.js
+++ javascript.js
@@ -1,29 +1,53 @@
-let randomNumber = Math.random();
+let randomNumber = 0;
 let computerSelection = '';
-let playerSelection = prompt('Do you choose Rock, Paper or Scissors?', '')
+let playerSelection = '';
+let playerWin = 0;
+let computerWin = 0;
 
 function getComputerChoice() {
     if (randomNumber < 0.333) {
-        computerSelection = 'Rock';
+        return computerSelection = 'Rock';
     } else if (randomNumber >= 0.666) {
-        computerSelection = 'Scissor';
+        return computerSelection = 'Scissor';
     } else {
-        computerSelection = 'Paper';
+        return computerSelection = 'Paper';
     }
 }
 
 getComputerChoice();
 
 function playRound(playerSelection, computerSelection) { 
+    randomNumber = Math.random();
+    computerSelection = getComputerChoice();
+    playerSelection = prompt('Do you choose Rock, Paper or Scissors?', '');
+
+    while (playerSelection.toLowerCase() !== 'rock' && playerSelection.toLowerCase() !== 'paper' && playerSelection.toLowerCase() !== 'scissor') {
+        playerSelection = prompt('This is not an applicable choice. Please choose either Rock, Paper or Scissors?', '');
+    }
+
     if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) { 
         return 'It\'s a tie!';
     } else if (playerSelection.toLowerCase() === 'rock' & computerSelection.toLowerCase() === 'scissor' || 
         playerSelection.toLowerCase() === 'paper' & computerSelection.toLowerCase() === 'rock' || 
         playerSelection.toLowerCase() === 'scissor' & computerSelection.toLowerCase() === 'paper') { 
-        return `You Win! ${playerSelection.slice(0, 1).toUpperCase()}${playerSelection.slice(1).toLowerCase()} beats ${computerSelection}`;
+            playerWin += 1;
+            return `You Win This Round! ${playerSelection.slice(0, 1).toUpperCase()}${playerSelection.slice(1).toLowerCase()} beats ${computerSelection}`;
     } else { 
-        return `You Lose! ${computerSelection} beats ${playerSelection.slice(0, 1).toUpperCase()}${playerSelection.slice(1).toLowerCase()}`;
+        computerWin += 1;
+        return `You Lose This Round! ${computerSelection} beats ${playerSelection.slice(0, 1).toUpperCase()}${playerSelection.slice(1).toLowerCase()}`;
     }
 }
 
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    for (let round = 1; round <= 5; round++){    
+        console.log( playRound(playerSelection, computerSelection) + ` Round ${round} Score: ${playerWin} (You) - ${computerWin} (Computer)`);
+    }
+
+    if (playerWin > computerWin){
+        console.log(`You've Won the Game! Score: ${playerWin} (You) - ${computerWin} (Computer)`)
+    } else {
+        console.log(`You've Lost the Game! Score: ${playerWin} (You) - ${computerWin} (Computer)`)
+    }
+}
+
+game();
